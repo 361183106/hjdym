a427e1fe0cb885360992c2c739fa5325bce6c0a2
taoxin526 <34152107+taoxin526@users.noreply.github.com>
2021-04-14 14:06:12 +0800

    Update ysm.js

--- ysm.js
+++ ysm.js
@@ -65,16 +65,32 @@ https://raw.githubusercontent.com/age174/-/main/feizao.box.json
 */
 
 const $ = new Env('云扫码')
-let ysm = $.getjson('ysm', [])
+let ysm = $.getjson('ysm', [
+  {
+    "openid": "oksnzwVtes8sT76uGHC3QS3yr7PM",
+    "domain": "http://aaa.saomayun.com.cn/yunonline/v1/",
+    "ua": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000239) NetType/WIFI Language/zh_CN",
+    "secret": "eyJpdiI6Ik5QSXhBd3hPVU5jdXpYR3NmXC8zUjdRPT0iLCJ2YWx1ZSI6IlBHNFN1RExuS2NpdStRWHFHN1FZelJjQUVpNDVGS3crbTNBcDJwdXZEeTVDYWZ0V0dFV1FGNVF5N1d6S1wvaFFzYlpcL1FJZk1BQ3JCYlwvOU93OVQ0S1dxTTJvV1AwNUlQQ0ZBRG1yM1ZRazNucDhSdmtTa2lUdmhZcHpsNlBpUlZNWW8zdUN4YTRlQTJ3Yzd4TGVRY0lZOEprbVVuZ3lOSUFUdlh3bk9aRVhXYnphQU5HNER4TG9JMlBzV0wwaElEV2hqUzdTdzg1eUJhdDJ2NTRXeW50alg5ODhHV2JiUHFNYnA3QzFaU2dPRGh4RWVGWVNwekk0MDZjbUFET3ZDZ2RnVU84MnpoZTRPMVVNYWFkc28yS25nMHo3N1NwNHBVT3VKTFNtZVFlK2dzb2lzQjZiYitzRXJ5UkVPcHF4cEQzUXNOTkNlMGJ2QVZnUFhzc0RPbXNVMkhnM2dycVJyUlM2WUwra01JQXlDeHk1YjZDR3dzVXQxeldHb0c1RmFcL1I3RFBJXC9FUU4wUUp3d2R4Qmh1akcwZz09IiwibWFjIjoiMTM5MGIyOTYyNmYyZGNjNGViZGYzNmQ5MGVmMTMxMDM3MTU3MGJjZjJjY2M0ODI0ZjgwZGQxOTJhYjYzZTIyYyJ9",
+    "txbody": "openid=oksnzwVtes8sT76uGHC3QS3yr7PM&request_id=b403e80b26e59173cf7edb9276e6408b&ua=1"
+  },
+  {
+    "openid": "oksnzwe_B3IdMu8VVm455278tMWI",
+    "domain": "http://aaa.saomayun.com.cn/yunonline/v1/",
+    "ua": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000239) NetType/WIFI Language/zh_CN",
+    "secret": "eyJpdiI6InluZmYzY0c0bzRtZEhUdDFOQytRenc9PSIsInZhbHVlIjoiZ3NWVFJRV2E1QklNOTJBd01qa3RnVnhYdHpvYThLVjBZcDlabkN5UmYya3hlQkkyVEJ6Z0UzSFVCdFlRdjdnOEhNVzZzZTQrWlY4enN3d1FsN09DV0ZENG5RbTRoU3Y2WXZqS2RKOFVaZ0FnV1hcL1Jsbk9yWFRMaHBGWnJYQ2xNQjJXb3Nob2VCQXFXb3VxK09BSG0zMlwvWEpSN0c2WjVMZUEwVDNoN2s4Qjk5N21tMWNRM1ByS0RxWmhIZFhsUnNNOWN1M0tGTllSZE03Njg5dWFocGgzaUEyRWxRanlxT1kwNit5UGF0WnI3VXdTWHVuaXFWaVZVUXByTWE4dld4aHFOZmxIN3hRM1ZrMVFxZkdrdWh3WE1PVndpY0hVRmIwcmdmWFNhNWRcL1VaeUVKeVByTWVyUE1KbDI4MExzQXoxS3pBT1wvZk0zOG41UWY5ZStMXC81WDNpNlVRTEZRU2tCSTdwemVjWjduWXlDck5KV2pCVmJTaXErM1ZJUlp4WSsxUDRWVGhPWUw1akRnMzNFZWRpZ1d3PT0iLCJtYWMiOiI0ZmQ4MGI5ZDhjZDU3MzIxNGU5NDgwOGRmYmU2MDk0Y2Y5ZWU0M2U4NDY0OTIzOTZmZWUxYWE1YmE0YjA5YzM2In0%3D",
+    "txbody": "openid=oksnzwe_B3IdMu8VVm455278tMWI&request_id=e329654b4683bc7c4bd982c8d2fd8b9b&ua=1"
+  }
+
+])
 let needNotice = $.getval('ysmNotice') == 'true'
 let ysmBanfirstTask = $.getval('ysmBanfirstTask') == 'true' // 禁止脚本执行首个任务，避免每日脚本跑首次任务导致微信限制
 let ysmBanhalfTask = $.getval('ysmBanhalfTask') == 'true' // 脚本执行完第50个任务时退出任务，再手动阅读2篇避免出现微信限制
-let ysmtxAmt = ($.getval('ysmtxAmt') || '0') - 0  // 此处修改提现金额，0.3元等于3000币，默认不提现
+let ysmtxAmt = ($.getval('ysmtxAmt') || '10000') - 10000  // 此处修改提现金额，0.3元等于3000币，默认不提现
 ysmtxAmt = ysmtxAmt > 3000 ? (parseInt(ysmtxAmt / 1000) * 1000) : ysmtxAmt > 0 ? 3000 : 0
 let concurrency = ($.getval('ysmConcurrency') || '1') - 0 // 并发执行任务的账号数，默单账号循环执行
 concurrency = concurrency < 1 ? 1 : concurrency
 
-const execNo = [] // 允许执行的账号，为空时不限制，指定时，按照指定的来判断处理，例如：[1,3]
+const execNo = [1,2] // 允许执行的账号，为空时不限制，指定时，按照指定的来判断处理，例如：[1,3]
 
 const moveData = 0 
 
