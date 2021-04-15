355df1093fc88b24242b575f52a9d00d4169fb4f
taoxin526 <34152107+taoxin526@users.noreply.github.com>
2021-04-15 16:47:14 +0800

    Update yqnb.js

--- yqnb.js
+++ yqnb.js
@@ -61,14 +61,14 @@ notifyInterval = 2; // 0为关闭通知，1为所有通知，2为12 23 点通知
 Minutes = 10; // 通知 默认控制在0-10分内
 $.message = '', COOKIES_SPLIT = '', CASH = '', XH = 0, Length = 0, ddtime = '';
 
-let yqnbheaderArr = [BJYADMIN=nk0hcqd8v03pathr8i26jco7d5];
-let yqnbheaderVal = ``;
-let middleyqnbHEADER = [BJYADMIN=nk0hcqd8v03pathr8i26jco7d5];
+let yqnbheaderArr = [];
+let yqnbheaderVal = `BJYADMIN=nk0hcqd8v03pathr8i26jco7d5`;
+let middleyqnbHEADER = [];
 
 
-let yqnbbodyArr = [9268];
-let yqnbbodyVal = ``;
-let middleyqnbBODY = [9268];
+let yqnbbodyArr = [];
+let yqnbbodyVal = `9268`;
+let middleyqnbBODY = [];
 
 
 if ($.isNode() && process.env.YQNB_yqnbHEADER) {
